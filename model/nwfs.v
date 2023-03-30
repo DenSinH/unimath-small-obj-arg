a7fa0d1f7a83748e938d124807ba64c12b5248a7
Dennis H <dhilhorst2000@gmail.com>
2023-03-30 20:17:41 +0200

    use SIP instead

--- model/nwfs.v
+++ model/nwfs.v
@@ -8,6 +8,8 @@ Require Import UniMath.CategoryTheory.whiskering.
 Require Import UniMath.CategoryTheory.Monads.Monads.
 
 Require Import UniMath.CategoryTheory.DisplayedCats.Core.
+(* The Structure Identity Principle (HoTT book, chapter 9.8) *)
+Require Import UniMath.CategoryTheory.DisplayedCats.SIP.
 Require Import UniMath.CategoryTheory.DisplayedCats.Total.
 Require Import UniMath.CategoryTheory.DisplayedCats.Functors.
 
@@ -24,46 +26,26 @@ Context (C:category).
 
 Definition arrow_base := category_binproduct C C.
 
-Definition arrow_disp_ob_mor : disp_cat_ob_mor arrow_base.
+Definition arrow_disp : disp_cat arrow_base.
 Proof.
-  use make_disp_cat_ob_mor.
-  (* objects are morphisms in original category *)
+  use disp_cat_from_SIP_data.
   - exact (λ xy, pr1 xy --> pr2 xy).
-  - (* for every two objects in the base category, and morphism between them,
-       a morphism in the displayed category *)
-    simpl; intros xx' yy' g h ff'.
+  - simpl.
+    intros xx' yy' g h ff'.
     exact (pr1 ff' · h = g · pr2 ff').
+  - simpl.
+    intros.
+    use homset_property.
+  - simpl. 
+    intros.
+    now rewrite id_left, id_right.
+  - simpl.
+    intros.
+    rewrite assoc, <- X.
+    symmetry.
+    now rewrite <- assoc, <- X0, assoc.
 Defined.
 
-Definition arrow_id_comp : disp_cat_id_comp _ arrow_disp_ob_mor.
-Proof.
-  split.
-  - (* identity in base category maps to identity in displayed category *)
-    simpl; intros.
-    etrans. apply id_left. apply pathsinv0, id_right.
-  - (* composition in base category maps to composition in displayed category *)
-    simpl; intros x y z f g xx yy zz ff gg.
-    etrans. apply @pathsinv0, assoc.
-    etrans. apply maponpaths, gg.
-    etrans. apply assoc.
-    etrans. apply cancel_postcomposition, ff.
-    apply pathsinv0, assoc.
-Qed.
-
-Definition arrow_data : disp_cat_data _
-  := (arrow_disp_ob_mor ,, arrow_id_comp).
-
-Lemma arrow_axioms : disp_cat_axioms _ arrow_data.
-Proof.
-  (* left / right identity / associativity / homsets *)
-  repeat apply tpair; intros; try apply homset_property.
-  apply isasetaprop, homset_property.
-Qed.
-
-Definition arrow_disp : disp_cat _
-  := (arrow_data ,, arrow_axioms).
-
-  
 Definition arrow : category := total_category arrow_disp.
 
 End Arrow_Disp.
@@ -76,56 +58,31 @@ Section Three_disp.
 Context (C:category).
 
 Definition three_base := (category_binproduct C (category_binproduct C C)).
-
-(* Can't do it over the arrow category, since this is a displayed category, not 
-an actual category *)
-Definition three_disp_ob_mor : disp_cat_ob_mor three_base.
+Definition three_disp : disp_cat three_base.
 Proof.
-  use make_disp_cat_ob_mor.
+  use disp_cat_from_SIP_data.
   - exact (λ xyz, pr1 xyz --> pr12 xyz × pr12 xyz --> pr22 xyz).
   - (* double commutative square *)
     simpl; intros xxx yyy gg hh fff.
     exact ((pr1 fff · pr1 hh = pr1 gg · pr12 fff) × (pr12 fff · pr2 hh = pr2 gg · pr22 fff)).
+  - simpl.
+    intros.
+    apply isapropdirprod; use homset_property.
+  - simpl. 
+    intros.
+    split; now rewrite id_left, id_right.
+  - simpl.
+    intros.
+    destruct X as [X1 X2].
+    destruct X0 as [Y1 Y2].
+    split.
+    * rewrite assoc, <- X1.
+      symmetry.
+      now rewrite <- assoc, <- Y1, assoc.
+    * rewrite assoc, <- X2.
+      symmetry.
+      now rewrite <- assoc, <- Y2, assoc.
 Defined.
-
-Definition three_id_comp : disp_cat_id_comp _ three_disp_ob_mor.
-Proof.
-  split.
-  - (* identity in base category maps to identity in displayed category *)
-    simpl; intros.
-    split; eapply pathscomp0.
-    * apply id_left.
-    * apply pathsinv0, id_right.
-    * apply id_left.
-    * apply pathsinv0, id_right.
-  - (* composition in base category maps to composition in displayed category *)
-    simpl; intros x y z ff gg xxx yyy zzz fff ggg.
-    split; eapply pathscomp0.
-    * apply @pathsinv0, assoc.
-    * eapply pathscomp0. apply maponpaths, ggg.
-      eapply pathscomp0. apply assoc.
-      eapply pathscomp0. apply cancel_postcomposition, fff.
-      apply pathsinv0, assoc.
-    * apply @pathsinv0, assoc.
-    * eapply pathscomp0. apply maponpaths, ggg.
-      eapply pathscomp0. apply assoc.
-      eapply pathscomp0. apply cancel_postcomposition, fff.
-      apply pathsinv0, assoc.
-Qed.
-
-Definition three_data : disp_cat_data _
-  := (three_disp_ob_mor ,, three_id_comp).
-
-Lemma three_axioms : disp_cat_axioms _ three_data.
-Proof.
-  (* left / right identity / associativity / homsets *)
-  repeat apply tpair; intros; try (apply isapropdirprod; apply homset_property).
-  apply isasetaprop.
-  apply isapropdirprod; apply homset_property.
-Qed.
-
-Definition three_disp : disp_cat _
-  := (three_data ,, three_axioms).
 
 Definition three : category := total_category three_disp.
 
@@ -148,9 +105,9 @@ Proof.
     * intros xxx yyy fff.
       exact (make_dirprod (pr12 fff) (pr22 fff)).
   - split.
-    * intro xxx.
+    * unfold functor_idax; intros.
       apply binprod_id.
-    * intros xxx yyy zzz ff gg.
+    * unfold functor_compax; intros.
       apply binprod_comp.
 Defined.
 
@@ -163,9 +120,9 @@ Proof.
     * intros xxx yyy fff.
       exact (make_dirprod (pr1 fff) (pr22 fff)).
   - split.
-    * intro xxx.
+    * unfold functor_idax; intros.
       apply binprod_id.
-    * intros xxx yyy zzz ff gg.
+    * unfold functor_compax; intros.
       apply binprod_comp.
 Defined.
 
@@ -178,9 +135,9 @@ Proof.
     * intros xxx yyy fff.
       exact (make_dirprod (pr1 fff) (pr12 fff)).
   - split.
-    * intro xxx.
+    * unfold functor_idax; intros.
       apply binprod_id.
-    * intros xxx yyy zzz ff gg.
+    * unfold functor_compax; intros.
       apply binprod_comp.
 Defined.
 
@@ -241,6 +198,7 @@ Definition face_map_0_disp : disp_functor face_map_0_base (three_disp C) (arrow_
 Proof.
   use tpair.
   - exact face_map_0_functor_data.
+  (* todo: this is still a bit vague to me *)
   (* can use homset_property of C to show that the morphisms are indeed equal *)
   - repeat split; intros; apply C.
 Defined.
@@ -334,9 +292,9 @@ Definition fact_functor {C : category} (F : functorial_factorization C) := pr1 F
 Coercion fact_functor : functorial_factorization >-> functor.
 Definition fact_cond {C : category} (F : functorial_factorization C) := pr2 F.
 
-Definition factorization_L {C : category} (F : functorial_factorization C) :=
+Definition fact_L {C : category} (F : functorial_factorization C) :=
     F ∙ face_map_2.
-Definition factorization_R {C : category} (F : functorial_factorization C) :=
+Definition fact_R {C : category} (F : functorial_factorization C) :=
     F ∙ face_map_0.
 
 Lemma fact_preserves_dom {C : category} (F : functorial_factorization C) (f : arrow C) :
@@ -359,10 +317,10 @@ Defined.
 Lemma LR_compatibility {C : category} (F : functorial_factorization C) (f : arrow C) : 
    Type.
 (* coq does not deduce that the resulting morphism has the same domain/codomain *)
-(* pr2 (factorization_R F f) ∘ pr2 (factorization_L F f) = (pr2 f). *)
+(* pr2 (fact_R F f) ∘ pr2 (fact_L F f) = (pr2 f). *)
 Proof.
-  (* (factorization_R F f) ∘ (factorization_L F f) = (pr1 F f). *)
-  set (lr := pr2 (factorization_R F f) ∘ pr2 (factorization_L F f)).
+  (* (fact_R F f) ∘ (fact_L F f) = (pr1 F f). *)
+  set (lr := pr2 (fact_R F f) ∘ pr2 (fact_L F f)).
   set (id := pr2 f).
   simpl in id.
   simpl in lr.
