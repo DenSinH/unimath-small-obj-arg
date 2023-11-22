c036c3392f3a02db42e4c5e705392750c9bdf898
Dennis <randomspam1906@gmail.com>
2023-11-22 11:39:43 +0100

    more renaming

--- ModelCategories/WeakEquivalences.v
+++ ModelCategories/WeakEquivalences.v
@@ -9,33 +9,33 @@ Local Open Scope cat.
 Local Open Scope morcls.
 Local Open Scope logic.
 
-Definition weq_comp_ax {M : category} (W : morphism_class M) : UU :=
+Definition weq_comp_ax {C : category} (W : morphism_class C) : UU :=
     ∀ x y z (f : x --> y) (g : y --> z), 
       (W _ _) f ⇒ (W _ _) g ⇒ (W _ _) (f · g).
-Definition weq_cancel_left_ax {M : category} (W : morphism_class M) : UU :=
-    ∀ (x y z : M) (f : x --> y) (g : y --> z), 
+Definition weq_cancel_left_ax {C : category} (W : morphism_class C) : UU :=
+    ∀ (x y z : C) (f : x --> y) (g : y --> z), 
       (W _ _) f ⇒ (W _ _) (f · g) ⇒ (W _ _) g.
-Definition weq_cancel_right_ax {M : category} (W : morphism_class M) : UU :=
-    ∀ (x y z : M) (f : x --> y) (g : y --> z), 
+Definition weq_cancel_right_ax {C : category} (W : morphism_class C) : UU :=
+    ∀ (x y z : C) (f : x --> y) (g : y --> z), 
       (W _ _) g ⇒ (W _ _) (f · g) ⇒ (W _ _) f.
 
-Definition is_weak_equivalences {M : category} (W : morphism_class M) : UU :=
+Definition is_weak_equivalences {C : category} (W : morphism_class C) : UU :=
     weq_comp_ax W × weq_cancel_left_ax W × weq_cancel_right_ax W.
 
-Definition weak_equivalences (M : category) : UU :=
-    ∑ (W : morphism_class M), is_weak_equivalences W.
+Definition weak_equivalences (C : category) : UU :=
+    ∑ (W : morphism_class C), is_weak_equivalences W.
 
-Definition weq_class {M : category} (W : weak_equivalences M) := pr1 W.
-Definition weq_is_weq {M : category} (W : weak_equivalences M) := (pr2 W).
-Definition is_weq_comp {M : category} {W : morphism_class M} (weq : is_weak_equivalences W) := (pr1 (weq)).
-Definition is_weq_cancel_left {M : category} {W : morphism_class M} (weq : is_weak_equivalences W) := pr12 weq.
-Definition is_weq_cancel_right {M : category} {W : morphism_class M} (weq : is_weak_equivalences W) := pr22 weq.
+Definition weq_class {C : category} (W : weak_equivalences C) := pr1 W.
+Definition weq_is_weq {C : category} (W : weak_equivalences C) := (pr2 W).
+Definition is_weq_comp {C : category} {W : morphism_class C} (weq : is_weak_equivalences W) := (pr1 (weq)).
+Definition is_weq_cancel_left {C : category} {W : morphism_class C} (weq : is_weak_equivalences W) := pr12 weq.
+Definition is_weq_cancel_right {C : category} {W : morphism_class C} (weq : is_weak_equivalences W) := pr22 weq.
 
-Definition weq_comp {M : category} (W : weak_equivalences M) := is_weq_comp (weq_is_weq W).
-Definition weq_cancel_left {M : category} (W : weak_equivalences M) := is_weq_cancel_left (weq_is_weq W).
-Definition weq_cancel_right {M : category} (W : weak_equivalences M) := is_weq_cancel_right (weq_is_weq W).
+Definition weq_comp {C : category} (W : weak_equivalences C) := is_weq_comp (weq_is_weq W).
+Definition weq_cancel_left {C : category} (W : weak_equivalences C) := is_weq_cancel_left (weq_is_weq W).
+Definition weq_cancel_right {C : category} (W : weak_equivalences C) := is_weq_cancel_right (weq_is_weq W).
 
-Lemma isaprop_is_weak_equivalences {M : category} (W : morphism_class M) : isaprop (is_weak_equivalences W).
+Lemma isaprop_is_weak_equivalences {C : category} (W : morphism_class C) : isaprop (is_weak_equivalences W).
 Proof.
   idtac; 
     repeat apply isapropdirprod; 
